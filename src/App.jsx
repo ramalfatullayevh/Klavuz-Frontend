d8d56a09e3153bf9c0220aae7ecfe5440d97b078
ramalfatullayevh <ramalfatullayevh@gmail.com>
2025-09-25 00:15:07 +0400

    secondss

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
