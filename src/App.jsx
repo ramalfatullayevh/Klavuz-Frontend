48e939f12bfaab93a9d8d9e7a36b5c4cb7cbba71
ramalfatullayevh <ramalfatullayevh@gmail.com>
2025-09-27 00:11:32 +0400

    error1

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import Register from "./Register";
 import Login from "./Login";
 import Error from "./Error";
-import { BrowserRouter, Route, Routes } from "react-router-dom"; 
+import { HashRouter as Router, Route, Routes } from "react-router-dom"; 
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<Login />} />
         <Route path="/error" element={<Error />} />
-\        <Route path="*" element={<Error />} />
+        <Route path="*" element={<Error />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
